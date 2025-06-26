1646eed97f77e03f818f120ac9fef7059f52d7ec
1Ibanok <lolkek999777666@gmail.com>
2025-06-27 02:50:27 +0300

    Add .gitignore to exclude build files

--- build/CMakeFiles/NetF.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for NetF.
-# This may be replaced when dependencies are built.
--- build/Desktop-Profile/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_NetF_FILE /home/shluha/projects/Netf/build/Desktop-Profile/NetF)
-set(__QT_DEPLOY_TARGET_NetF_TYPE EXECUTABLE)
